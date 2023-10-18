396865b2212bbacb7e0e72a7844b39117beab51d
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2023-10-18 21:05:54 +0300

    sdm845-common: build libprotobuf-cpp-full-3.9.1 and ship prebuilt libprotobuf-cpp-full
    * libprotobuf-cpp-full-vendorcompat is no longer a thing

--- sdm845.mk
+++ sdm845.mk
@@ -344,6 +344,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
+
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
