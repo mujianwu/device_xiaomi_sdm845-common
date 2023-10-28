8d31ec98bf82e62c395ee4fa0390b52ff2c3426b
snnbyyds <snnbyyds@gmail.com>
2023-10-28 18:36:31 +0800

    sdm845-common: Build Camera2 Aperture is not available for now and Camera2 is not built by default. Thus let's simply build Camera2 so that we can have a usable camera APP.
    Signed-off-by: mujianwu <lrh15563910083@163.com>
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -114,7 +114,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Aperture \
+    Camera2 \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl:32 \
