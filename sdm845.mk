6c14be5637640075b4a80913bc505490a9665617
chara <blocklu@163.com>
2023-11-03 23:16:33 +0800

    sdm845-common: Build vendor.qti.hardware.btconfigstore@1.0.vendor & vendor.qti.hardware.capabilityconfigstore@1.0.vendor

--- sdm845.mk
+++ sdm845.mk
@@ -111,7 +111,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl:32 \
     audio.bluetooth.default \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
